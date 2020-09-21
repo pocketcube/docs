ba491b3e3fb1f51d1385beb198a5098f200b991b
Andrew Lucas <andrewlucasgs@gmail.com>
2020-09-21 00:22:21 -0300

    Add tables

--- conf.py
+++ conf.py
@@ -22,7 +22,7 @@ from recommonmark.transform import AutoStructify
 
 # -- Project information -----------------------------------------------------
 
-project = 'Satélite para Educação'
+project = 'Satélite para Educação Básica'
 author = 'Equipe PocketQube'
 
 
